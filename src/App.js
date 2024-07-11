f1d01ba7424126a922338fda8756e11a9ca14008
Waleed Sabir <waleed_sabir@yahoo.com>
2024-07-10 21:40:10 -0400

    Update code in App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is a simple React application to demonstrate Jenkins integration.
-        </p>
+        <p>React application with Jenkins integration.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
