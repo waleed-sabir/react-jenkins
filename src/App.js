8527bbf7fc7d4212108d60222b48ad1dc7756ca7
Waleed Sabir <waleed_sabir@yahoo.com>
2024-10-17 19:39:53 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn and Master React
+          Learn React
         </a>
       </header>
     </div>
